cc8379dd5a03c9ecad74e8e4d03bb19f416837cb
Noemi Baglieri <noemibaglieri@gmail.com>
2025-06-17 16:09:35 +0200

    Task 2 completed

--- src/task2/Task2.java
+++ src/task2/Task2.java
@@ -1,7 +1,22 @@
 package task2;
 
+import java.util.Scanner;
+
 public class Task2 {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Type a number:");
+        int inputNum = scanner.nextInt();
+
+        String value = switch (inputNum) {
+            case 1 -> "One";
+            case 2 -> "Two";
+            case 3 -> "Three";
+            default -> "Wrong number.";
+        };
+
+        System.out.println(value);
+
+        scanner.close();
     }
 }
